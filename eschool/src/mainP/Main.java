ffa1731949bf669bf5066f3821ace10f36c81ba7
SBAI SALAH <88663177+Sbai-Salah@users.noreply.github.com>
2022-06-08 14:27:37 +0100

    mainP added

---
+++ eschool/src/mainP/Main.java
@@ -0,0 +1,26 @@
+package mainP;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+// Starting point of Application  (main function)
+public class Main extends Application {
+
+    Stage window;
+    Scene scene1,scene2;
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+       System.out.println("The app is working");
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        primaryStage.setTitle("E-School");
+        primaryStage.setScene(new Scene(root, 1020, 580));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
