67a4055a151b3f79ec03ed2d6da71d7dd3bfd74d
SBAI SALAH <88663177+Sbai-Salah@users.noreply.github.com>
2022-06-08 14:26:30 +0100

    student Pushed

---
+++ eschool/src/sController/Attendance.java
@@ -0,0 +1,103 @@
+package sController;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+// Student Attendence
+public class Attendance {
+    @FXML
+    void logout(MouseEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/mainP/login.fxml"));
+
+        Node node = (Node) event.getSource();
+
+        Stage stage = (Stage) node.getScene().getWindow();
+
+        stage.setScene(new Scene(root));
+
+    }
+    @FXML
+    void dash(MouseEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/sView/homeStudent.fxml"));
+
+        Node node = (Node) event.getSource();
+
+        Stage stage = (Stage) node.getScene().getWindow();
+
+        stage.setScene(new Scene(root));
+
+    }
+    @FXML
+    void homei(MouseEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/sView/homeview.fxml"));
+
+        Node node = (Node) event.getSource();
+
+        Stage stage = (Stage) node.getScene().getWindow();
+
+        stage.setScene(new Scene(root));
+
+    }
+    @FXML
+        void courseReg(MouseEvent event) throws IOException {
+            Parent root = FXMLLoader.load(getClass().getResource("/sView/courseRegistration.fxml"));
+
+            Node node = (Node) event.getSource();
+
+            Stage stage = (Stage) node.getScene().getWindow();
+
+            stage.setScene(new Scene(root));
+
+    }
+    @FXML
+    void st_marks(MouseEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/sView/Student_Marks_C.fxml"));
+
+        Node node = (Node) event.getSource();
+
+        Stage stage = (Stage) node.getScene().getWindow();
+
+        stage.setScene(new Scene(root));
+
+    }
+
+    @FXML
+    void transcript(MouseEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/sView/transcript.fxml"));
+
+        Node node = (Node) event.getSource();
+
+        Stage stage = (Stage) node.getScene().getWindow();
+
+        stage.setScene(new Scene(root));
+
+    }
+    @FXML
+    void studyPlan(MouseEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/sView/semester1.fxml"));
+
+        Node node = (Node) event.getSource();
+
+        Stage stage = (Stage) node.getScene().getWindow();
+
+        stage.setScene(new Scene(root));
+
+    }
+    @FXML
+    void feeDe(MouseEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/sView/feeDetails.fxml"));
+
+        Node node = (Node) event.getSource();
+
+        Stage stage = (Stage) node.getScene().getWindow();
+
+        stage.setScene(new Scene(root));
+
+    }
+}
